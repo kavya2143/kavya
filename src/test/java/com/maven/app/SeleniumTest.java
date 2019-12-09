5ee21e145beffe7dd5e815801ac874f11561acd4
kavya <kavyadatla1@gmail.com>
2019-12-09 13:41:52 +0100

    first commit

---
+++ src/test/java/com/maven/app/SeleniumTest.java
@@ -0,0 +1,13 @@
+package com.maven.app;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public static void  sel() {
+		System.out.println("kavya into maven ");
+	}
+	
+
+}
